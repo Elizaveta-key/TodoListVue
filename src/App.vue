3c0f16fee5d458efdaef7052fe399cbdf9d567d0
Elizaveta Kush <101804454+Elizaveta-key@users.noreply.github.com>
2023-09-10 18:37:25 +0300

    TodoListVue

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<!-- //содержит базовый html и javascript для приложения. -->
+<script setup>
+import TodoList from './TodoList.vue';
+</script>
+
+<template>
+    <main>
+        <h1>Todo List/Лист задач</h1>
+        <TodoList/>
+    </main>
+</template>
+
+<style scoped>
+    main {
+        font-family: Verdana, sans-serif;
+    }
+
+    h1 {
+        font-weight: bold; 
+        color: rgb(84, 79, 79);
+        padding: 8px 16px;
+        background-color: rgb(212, 203, 203);
+        border: solid rgb(237, 233, 233);
+        border-radius: 8px;
+        margin-bottom: 16px;
+        text-align: center;
+        font-style:italic;
+    }
+</style>
---
+++ src/TodoCard.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="todo">
+      <div class="container">
+        <div class="row">
+          <input class="check" type="checkbox" />
+          <h3 class="col strikethrough"
+          @dblclick="editTodo(todo)">{{ todo.text }}</h3>
+          <div v-show="todo.edit == false">
+            <input v-show="todo.edit == true"
+            v-model="todo.title"
+            v-on:blur="todo.edit=false; $emit('update')"
+            @keyup.enter="todo.edit=false; $emit('update')">
+          </div>
+          <hr>
+          <button @click="removeTodo"
+          type="button"
+          class="btn btn-danger btn-sm">Delete</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['todo'],
+    methods: {
+      removeTodo: function (todo) {
+        this.$emit('remove')
+      },
+      editTodo: function (todo) {
+        this.editedTodo = todo
+      }
+    }
+  }
+  </script>
